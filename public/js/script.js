fcea380199f1de3b660a963a889848790102e9f9
Gabriel Ferreira <gf-castro2012@hotmail.com>
2024-10-10 21:45:00 -0300

    :fix: fix: Corrigindo bugs do front

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 const paragrafo = document.getElementById('sobreMim');
-const textoCompleto = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta asperiores consequuntur doloremque, minima veniam vel fuga,nihil, nisi nulla reprehenderit iusto vitae beatae enim corporis odio maiores molestias! Optio, omnis!Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta asperiores consequuntur doloremque, minima veniam vel fuga,nihil, nisi nulla reprehenderit iusto vitae beatae enim corporis odio maiores molestias! Optio, omnis!";
+const textoCompleto = "Sou estudante do 4º Período de Ciência da Computação na Universidade Federal de Lavras, onde já fui participante durante 1 ano da Empresa Júnior Comp Jr. Nela, fui responsalvel pelo desenvolvimento back-end, sendo o único nessa área do projeto do site da Juridica Jr. Também realizo Iniciação Científica, pela FAPEMIG, onde estou desenvolvendo uma ferramenta, resposável por gerenciar avalições de acessibilidade em sites da Web. Nesse projeto sou FullStack, e utilizo o framework Laravel, do PHP. Tenho mais conhecimento nas linguagens Python, PHP e JavaScript. No caso do Python, possuo habilidades em análise de dados, tendo um bom domínio da biblioteca Pandas.";
 let index = 0;
 
 function escreverTexto() {
